0f643d78c83c7312b5b5522ea33bbf4e49397e95
Dominik <janicekd0@gmail.com>
2018-02-05 18:47:44 +0100

    pridanie loginu, registracie a resetovcieho mailu

---
+++ app/src/main/java/cz/utb/fai/dodo/sharemyloc/NameSurnameActivity.java
@@ -0,0 +1,15 @@
+package cz.utb.fai.dodo.sharemyloc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NameSurnameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_surname);
+
+
+    }
+}
